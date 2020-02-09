7e127a27c9362aa1279e5170201147b77df9dd37
david <david@MacBook-Pro-de-david.local>
2020-02-10 00:42:16 +0100

    morning news part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+
+import ScreenHome from './ScreenHome';
+import ScreenArticlesBySource from './ScreenArticlesBySource'
+import ScreenMyArticles from './ScreenMyArticles'
+import ScreenSource from './ScreenSource'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Switch>
+        <Route component={ScreenHome} path="/" exact />
+        <Route component={ScreenSource} path="/screensource" exact />
+        <Route component={ScreenArticlesBySource} path="/screenarticlesbysource/:id" exact />
+        <Route component={ScreenMyArticles} path="/screenmyarticles" exact />
+      </Switch>
+    </Router>
+
   );
 }
 
